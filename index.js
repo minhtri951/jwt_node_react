4a1e96b6905ad976df3cb771ea6ff45bfd0beb92
Tri <vominhtri951@gmail.com>
2020-06-17 23:31:01 +0700

    Session login

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const port = 3000
 const { log } = console
 
 
-app.get('/', (req, res) => {
-    res.send('Hello!')
+app.get('/login', (req, res) => {
+    res.set({
+        'Set-Cookie': 'session=oRlkVLOkCBNrMilaSWnTcWtCfJC; path=/;',
+    })
+    res.redirect('/user')
 })
 
+app.get('/user', (req, res) => {
+    res.send('Logged! Hello user!')
+})
+
+
+app.get('/logout', (req, res) => {
+    res.set({
+        'Set-Cookie': 'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+    })
+    res.send('Loggouted!')
+})
 
 
 app.listen(port, () => {
